58b9992d7644f326a3f85a00c414405bf8f73c2a
lr <lv_alrily188@outlook.com>
2025-12-04 15:28:06 +0800

    updatename

--- src/app/(home)/hi-card.tsx
+++ src/app/(home)/hi-card.tsx
@@ -1,7 +1,7 @@
-import { useCenterStore } from '@/hooks/use-center'
 import Card from '@/components/card'
-import { useConfigStore } from './stores/config-store'
+import { useCenterStore } from '@/hooks/use-center'
 import { HomeDraggableLayer } from './home-draggable-layer'
+import { useConfigStore } from './stores/config-store'
 
 function getGreeting() {
 	const hour = new Date().getHours()
@@ -31,7 +31,7 @@ export default function HiCard() {
 			<Card order={styles.order} width={styles.width} height={styles.height} x={x} y={y} className='text-center max-sm:static max-sm:translate-0'>
 				<img src='/images/avatar.png' className='mx-auto rounded-full' style={{ width: 120, height: 120, boxShadow: ' 0 16px 32px -5px #E2D9CE' }} />
 				<h1 className='font-averia mt-3 text-2xl'>
-					{greeting} <br /> I'm <span className='text-linear text-[32px]'>Suni</span> , Nice to <br /> meet you!
+					{greeting} <br /> I'm <span className='text-linear text-[32px]'>Riny</span> , Nice to <br /> meet you!
 				</h1>
 			</Card>
 		</HomeDraggableLayer>
