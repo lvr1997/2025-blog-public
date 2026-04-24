3c40ce9784214171f65a83cfacb57ff44f1e2479
Riny <lvr1997@qq.com>
2026-04-24 15:18:30 +0800

    删除文章

--- src/components/like-button.tsx
+++ src/components/like-button.tsx
@@ -13,9 +13,9 @@ type LikeButtonProps = {
 	delay?: number
 }
 
-const ENDPOINT = 'https://blog-liker.yysuni1001.workers.dev/api/like'
+const ENDPOINT = 'https://blog-liker.lavrena.workers.dev/api/like'
 
-export default function LikeButton({ slug = 'yysuni', delay, className }: LikeButtonProps) {
+export default function LikeButton({ slug = 'rinyblog', delay, className }: LikeButtonProps) {
 	slug = BLOG_SLUG_KEY + slug
 	const [liked, setLiked] = useState(false)
 	const [show, setShow] = useState(false)
